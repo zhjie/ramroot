122bd939e798216f57b086691986dfdd7866a3c1
zhjie <smthzhj@gmail.com>
2024-03-03 14:46:16 +0800

    init

---
+++ gen_initramfs-rpi.sh
@@ -0,0 +1,21 @@
+mount /dev/mmcblk0p1 /boot
+
+rm -rfv /boot/overlays.bkp
+mv -v /boot/overlays            /boot/overlays.bkp
+mv -v /boot/bcm2711-rpi-4-b.dtb /boot/bcm2711-rpi-4-b.dtb.bkp
+mv -v /boot/bcm2711-rpi-cm4.dtb /boot/bcm2711-rpi-cm4.dtb.bkp
+
+rm -rfv /root/boot
+rsync -azL 192.168.1.20:/mnt/gentoo/boot/ /root/boot/
+cp -r /root/boot/* /boot/
+sync
+
+rm -rfv /root/initramfs
+mkdir /root/initramfs
+cd /root/initramfs
+cp /boot/initramfs ./initramfs.xz
+xz -d initramfs.xz
+cpio -iF initramfs
+rm initramfs
+cp /root/init ./init
+find . -print0 | cpio --quiet --null -o -H newc --owner root:root --force-local | xz -e --check=none -z -f -9 -T 0 > /boot/initramfs
---
+++ gen_initramfs-x86.sh
@@ -0,0 +1,21 @@
+mount /dev/sda1 /boot
+
+rm -rfv /root/boot
+rsync -azL 192.168.1.15:/mnt/bridge/boot/ /root/boot/
+rsync -azL /root/boot/ /boot/
+sync
+
+rm -rfv /root/initramfs
+mkdir /root/initramfs
+cd /root/initramfs
+cp /boot/initramfs ./initramfs.xz
+xz -d initramfs.xz
+cpio -iF initramfs
+rm initramfs
+cp /root/init ./init
+find . -print0 | cpio --quiet --null -o -H newc --owner root:root --force-local | xz -e --check=none -z -f -9 -T 0 > /boot/initramfs-6.7.9999-networkaudio-rt-x86_64.img
+cp /boot/System.map /boot/System.map-6.7.9999-networkaudio-rt-x86_64
+cp /boot/kernel /boot/vmlinuz-6.7.9999-networkaudio-rt-x86_64
+
+rm /boot/*.old
+grub-mkconfig -o /boot/grub/grub.cfg
